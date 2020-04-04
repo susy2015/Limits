3e61d2f1a4fdafc9892490292fd24c07cc0979e0
Matthew <mkilpatr@FNAL.GOV>
2020-04-04 14:34:30 -0500

    Making all of the datacards

--- manySignalDatacards.sh
+++ manySignalDatacards.sh
@@ -2,6 +2,6 @@
 
 while read p; do 
 
-	python Datacards/python/writeDatacard_SUSYNano19.py -l SMS_T1tttt_fastsim -s $p -m True
+	python Datacards/python/writeDatacard_SUSYNano19.py -l SMS_T2tt_fastsim_2016 -s $p -m True
 
 done < $1
