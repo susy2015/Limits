7d0dfbbf5a82d3c867cfd4d8e6be44898495fdfa
Matthew <mkilpatr@FNAL.GOV>
2020-05-27 15:51:37 -0500

    Change ll back to origin toptag

--- manySignalDatacards.sh
+++ manySignalDatacards.sh
@@ -2,6 +2,8 @@
 
 while read p; do 
 
-	python Datacards/python/writeDatacard_SUSYNano19.py -l SMS_T2tt_fastsim_2016 -s $p -m True
+	xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcsusyhad/Stop_production/LimitInputs/19May2020_Run2Unblind_dev_v6/SMS_T2tt_fastsim/${p}.json Datacards/setup/SUSYNano19/SMS_T2tt_fastsim/.
+	xrdcp root://cmseos.fnal.gov//eos/uscms/store/user/lpcsusyhad/Stop_production/LimitInputs/19May2020_Run2Unblind_dev_v6/SMS_T2tt_fastsim/${p}_syst.conf Datacards/setup/SUSYNano19/SMS_T2tt_fastsim/.
+	python Datacards/python/writeDatacard_SUSYNano19.py -l SMS_T2tt_fastsim -s $p -m True
 
 done < $1
