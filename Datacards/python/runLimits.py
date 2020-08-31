fb03ee00027203b27c3c1334eee143cf289485b6
Zhenbin Wu <zhenbin.wu@gmail.com>
2020-08-31 16:15:53 -0500

    Correct the code due to git commit
    Add back the smoothing code

--- Datacards/python/runLimits.py
+++ Datacards/python/runLimits.py
@@ -179,7 +179,6 @@ def getLimit(rootFile, getMean=False, limit={}):
 def printLimits(config):
     limits = []
     if not config.isEOS == '': currentDir = "/eos/uscms/store/user/"+getpass.getuser()+"/13TeV/"
-    #if not config.isEOS == '': currentDir = "/eos/uscms/store/user/benwu/13TeV/"
     else: currentDir = os.getcwd()
     for signal in config.signals:
         outputLocation = os.path.join(currentDir, config.limitdir, signal)
@@ -286,7 +285,6 @@ def fillSignificances(config, sigfile, name):
 def fillAsymptoticLimits(config, limfilename, excfilename, interpolate):
     limits = []
     if not config.isEOS == '': currentDir = "/eos/uscms/store/user/"+getpass.getuser()+"/13TeV/"
-    #if not config.isEOS == '': currentDir = "/eos/uscms/store/user/benwu/13TeV/"
     else: currentDir = os.getcwd()
     xsecfilename = ('Datacards/setup/xsecs/xSec.root')
     outfile = TFile(limfilename, 'RECREATE')
